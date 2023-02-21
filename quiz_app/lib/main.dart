199f41d5d9d0e8e8823c0bad3a728166d038bd59
David Chen <david@wearise.com>
2023-02-21 17:05:58 -0600

    minor changes

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,5 +1,19 @@
-void main() {
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
 }
 
-class MyApp {}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('Default text'),
+      ),
+    );
+  }
+}
